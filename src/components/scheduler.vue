b6fe0be8086d0b7fd47a9332c0a0c922a88cacaa
Jeremiah dufourq <jezdufourq@gmail.com>
2020-11-30 14:19:26 +1000

    update: formatting on the scheduler.

--- src/components/scheduler.vue
+++ src/components/scheduler.vue
@@ -6,46 +6,44 @@
                     Enter your scheduling details
                 </div>
                 <div>
-                    <q-input color="primary" v-model="person" label="Person to message" />
-                    <q-input color="primary" v-model="message" label="Message you would like to send" />
-                </div>
-                <div>
-                    <div class="q-pa-md" style="max-width: 300px">
-                        <q-input filled v-model="date">
-                        <template v-slot:prepend>
-                            <q-icon name="event" class="cursor-pointer">
-                            <q-popup-proxy transition-show="scale" transition-hide="scale">
-                                <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
-                                <div class="row items-center justify-end">
-                                    <q-btn v-close-popup label="Close" color="primary" flat />
-                                </div>
-                                </q-date>
-                            </q-popup-proxy>
-                            </q-icon>
-                        </template>
+                    <q-input class="q-py-md" color="primary" v-model="person" label="Person to message" />
+                    <q-input class="q-py-md" color="primary" v-model="message" label="Message you would like to send" />
+                    <q-input class="q-py-md" filled v-model="date">
+                    <template v-slot:prepend>
+                        <q-icon name="event" class="cursor-pointer">
+                        <q-popup-proxy transition-show="scale" transition-hide="scale">
+                            <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                            <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="primary" flat />
+                            </div>
+                            </q-date>
+                        </q-popup-proxy>
+                        </q-icon>
+                    </template>
 
-                        <template v-slot:append>
-                            <q-icon name="access_time" class="cursor-pointer">
-                            <q-popup-proxy transition-show="scale" transition-hide="scale">
-                                <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
-                                <div class="row items-center justify-end">
-                                    <q-btn v-close-popup label="Close" color="primary" flat />
-                                </div>
-                                </q-time>
-                            </q-popup-proxy>
-                            </q-icon>
-                        </template>
-                        </q-input>
-                    </div>
+                    <template v-slot:append>
+                        <q-icon name="access_time" class="cursor-pointer">
+                        <q-popup-proxy transition-show="scale" transition-hide="scale">
+                            <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
+                            <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="primary" flat />
+                            </div>
+                            </q-time>
+                        </q-popup-proxy>
+                        </q-icon>
+                    </template>
+                    </q-input>
                 </div>
             </q-card-section>
             <q-card-actions align="right">
-                <q-btn icon="add" color="primary"/>
+                <q-btn icon="add" color="primary" @click="submitData"/>
             </q-card-actions>
         </q-card>
     </div>
 </template>
 <script>
+
+import { date } from 'quasar'
 
 export default {
   name: 'scheduler',
@@ -53,7 +51,11 @@ export default {
     return {
       person: '',
       message: '',
-      date: Date.now()
+      date: date.formatDate(Date.now(), 'YYYY-MM-DD HH:mm')
+    }
+  },
+  methods: {
+    submitData () {
     }
   }
 }
