cd243a86cc0706e9f658c2bbeed64e3754bd3cb9
Jeremiah dufourq <jezdufourq@gmail.com>
2020-11-30 11:52:57 +1000

    add: card for the main component.

---
+++ src/components/scheduler.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <q-card>
+            <q-card-section>
+                <div class="text-h4 text-bold text-black">
+                    Enter your scheduling details
+                </div>
+            </q-card-section>
+            <q-card-actions align="right">
+                <q-btn icon="add" color="secondary"/>
+            </q-card-actions>
+        </q-card>
+    </div>
+</template>
+<script>
+export default {
+  name: 'scheduler'
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,15 @@
 <template>
   <q-page class="flex flex-center">
+    <scheduler />
   </q-page>
 </template>
 
 <script>
+import Scheduler from 'src/components/scheduler.vue'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    Scheduler
+  }
 }
 </script>
