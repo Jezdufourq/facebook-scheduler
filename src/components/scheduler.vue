1c841be4f02c7c299f3bb221b42dfe8721dfc224
Jeremiah dufourq <jezdufourq@gmail.com>
2020-11-30 12:01:58 +1000

    add: date to the scheduler.

--- src/components/scheduler.vue
+++ src/components/scheduler.vue
@@ -5,6 +5,39 @@
                 <div class="text-h4 text-bold text-black">
                     Enter your scheduling details
                 </div>
+                <div>
+                    <q-input color="secondary" v-model="person" label="Person to message" />
+                    <q-input color="secondary" v-model="message" label="Message you would like to send" />
+                </div>
+                <div>
+                    <div class="q-pa-md" style="max-width: 300px">
+                        <q-input filled v-model="date">
+                        <template v-slot:prepend>
+                            <q-icon name="event" class="cursor-pointer">
+                            <q-popup-proxy transition-show="scale" transition-hide="scale">
+                                <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                                <div class="row items-center justify-end">
+                                    <q-btn v-close-popup label="Close" color="primary" flat />
+                                </div>
+                                </q-date>
+                            </q-popup-proxy>
+                            </q-icon>
+                        </template>
+
+                        <template v-slot:append>
+                            <q-icon name="access_time" class="cursor-pointer">
+                            <q-popup-proxy transition-show="scale" transition-hide="scale">
+                                <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
+                                <div class="row items-center justify-end">
+                                    <q-btn v-close-popup label="Close" color="primary" flat />
+                                </div>
+                                </q-time>
+                            </q-popup-proxy>
+                            </q-icon>
+                        </template>
+                        </q-input>
+                    </div>
+                </div>
             </q-card-section>
             <q-card-actions align="right">
                 <q-btn icon="add" color="secondary"/>
@@ -13,7 +46,15 @@
     </div>
 </template>
 <script>
+
 export default {
-  name: 'scheduler'
+  name: 'scheduler',
+  data () {
+    return {
+      person: '',
+      message: '',
+      date: Date.now()
+    }
+  }
 }
 </script>
