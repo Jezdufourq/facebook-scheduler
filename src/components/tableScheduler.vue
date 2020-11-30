cdb83d58c828b5be65ad0a55b19d025a7cc4df2b
Jeremiah dufourq <jezdufourq@gmail.com>
2020-11-30 15:16:19 +1000

    update: added table + scheduler component into one component.

--- src/components/tableScheduler.vue
+++ src/components/tableScheduler.vue
@@ -20,33 +20,62 @@
           />
         </q-card>
         <q-dialog v-model="dialogOpen">
-          <scheduler />
+          <q-card>
+            <q-card-section>
+                <div class="text-h4 text-bold text-black">
+                    Enter your scheduling details
+                </div>
+                <div>
+                    <q-input class="q-py-md" color="primary" v-model="name" label="Name of the scheduler" />
+                    <q-input class="q-py-md" color="primary" v-model="person" label="Person to message" />
+                    <q-input class="q-py-md" color="primary" v-model="message" label="Message you would like to send" />
+                    <q-input class="q-py-md" filled v-model="date">
+                    <template v-slot:prepend>
+                        <q-icon name="event" class="cursor-pointer">
+                        <q-popup-proxy transition-show="scale" transition-hide="scale">
+                            <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                            <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="primary" flat />
+                            </div>
+                            </q-date>
+                        </q-popup-proxy>
+                        </q-icon>
+                    </template>
+
+                    <template v-slot:append>
+                        <q-icon name="access_time" class="cursor-pointer">
+                        <q-popup-proxy transition-show="scale" transition-hide="scale">
+                            <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
+                            <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="primary" flat />
+                            </div>
+                            </q-time>
+                        </q-popup-proxy>
+                        </q-icon>
+                    </template>
+                    </q-input>
+                </div>
+            </q-card-section>
+            <q-card-actions align="right">
+                <q-btn icon="add" color="primary" @click="submitData" />
+            </q-card-actions>
+        </q-card>
         </q-dialog>
     </div>
 </template>
 
 <script>
 import { date } from 'quasar'
-import Scheduler from './scheduler'
 
 export default {
   name: 'tableScheduler',
-  components: {
-    Scheduler
-  },
-  computed: {
-    data: {
-      get () {
-        return this.$store.getCurrentSchedulers
-      },
-      set (payload) {
-        this.store.commit('updateCurrentSchedulers', payload)
-      }
-    }
-  },
   data () {
     return {
       dialogOpen: false,
+      name: '',
+      person: '',
+      message: '',
+      date: date.formatDate(Date.now(), 'YYYY-MM-DD HH:mm'),
       columns: [
         {
           name: 'name',
@@ -71,6 +100,8 @@ export default {
           field: 'date',
           sortable: true
         }
+      ],
+      data: [
       ]
     }
   },
@@ -78,6 +109,24 @@ export default {
     openDialog () {
       this.dialogOpen = true
       console.log('dialog opened')
+    },
+    submitData () {
+      this.data.push({
+        name: this.name,
+        person: this.person,
+        date: this.date
+      })
+      console.log('obj: ' + JSON.stringify({
+        name: this.name,
+        person: this.person,
+        date: this.date
+      }))
+      // closing dialog + resetting data
+      this.openDialog = false
+      this.name = ''
+      this.person = ''
+      this.message = ''
+      this.date = null
     }
   }
 }
